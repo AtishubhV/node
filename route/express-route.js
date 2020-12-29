8f8387f6545c665a65fc560daf1ca1fcd03dc836
Vaishnaw, Atishubh [GTSUS NON-J&J] <avaishna@its.jnj.com>
2020-12-29 19:53:19 +0530

    Adding DB connection

--- route/express-route.js
+++ route/express-route.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const os = require("os");
+const mysql = require('mysql');
 const utilitis = require('../utility/utility');
 const products = [];
+var connection = mysql.createConnection({
+    host: 'mysqldb.c8zwxcxtn6tj.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'bobobobo',
+    database: 'Hogwarts'
+});
+
+connection.connect();
+
+
 router.use('/test', (req, res, next) => {
     res.send('Test');
 });
@@ -18,9 +29,17 @@ router.use('/home', (req, res, next) => {
     res.send('Home');
 });
 router.get('/products', (req, res, next) => {
+    connection.query('SELECT * from Students', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+        res.render('Products', { prds: results, hasProducts: true });
+        // connection.end();
+    });
+
+
 
     //res.sendFile(path.join(utilitis.getCurrentPath,'views','Products.html'));
-    res.render('Products');
+
 })
 router.use('/add-product', (req, res, next) => {
     res.send("<form method='POST' action='/product'><input name='product' type='text'></input><input value='ADD' type='submit'/></form>");
